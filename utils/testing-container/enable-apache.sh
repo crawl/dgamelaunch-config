3311def3d53cfe539499a3252347bbc2e228cfae
Rytis Petronis <petronisrytis@gmail.com>
2022-05-28 21:02:07 +0300

    add missing dir

--- utils/testing-container/enable-apache.sh
+++ utils/testing-container/enable-apache.sh
@@ -11,6 +11,7 @@ sudo a2enmod rewrite
 sudo a2dissite 000-default #remove apache default site because it interferes with port 80
 sudo a2ensite httpd #use the simplified crawl config for serving files on port 80 inside the container
 
+sudo mkdir /var/www/crawl/meta/
 sudo mkdir /var/www/crawl/meta/git/
 cd /var/www/crawl/meta/git/
 sudo ln -s $DGL_CHROOT/crawl-master/crawl-git/saves/logfile
