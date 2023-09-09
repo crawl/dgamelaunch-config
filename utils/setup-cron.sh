bff1821f2c951f32b4d4f7889bae13511e35c477
Rytis Petronis <petronisrytis@gmail.com>
2023-09-09 13:12:40 +0300

    update crontab

--- utils/setup-cron.sh
+++ utils/setup-cron.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# This script sets up three different cron jobs
-
 # Define the commands and their schedules
 command1="/home/crawl-dev/dgamelaunch-config/bin/dgl update-trunk"
 schedule1="*5 4 * * *"  # Runs daily at 04:05 AM
@@ -12,10 +10,19 @@ schedule2="5 3 * * *"     # Runs daily at 03:05 AM
 command3="/home/crawl-dev/dgamelaunch-config/bin/dgl update-gcc bcadrencrawl bcadrencrawl/bcrawl"
 schedule3="35 3 * * *"      # Runs daily at 03:35 AM
 
-# Add the commands and schedules to the crontab
-(crontab -l 2>/dev/null; echo "$schedule1 $command1") | crontab -
-(crontab -l 2>/dev/null; echo "$schedule2 $command2") | crontab -
-(crontab -l 2>/dev/null; echo "$schedule3 $command3") | crontab -
+# Check if a crontab file exists for the user, create one if not
+if [ ! -e "$HOME/crontab.txt" ]; then
+    touch "$HOME/crontab.txt"
+fi
+
+# Add the commands and schedules to the crontab file
+{ crontab -l 2>/dev/null; echo "$schedule1 $command1"; } >> "$HOME/crontab.txt"
+{ crontab -l 2>/dev/null; echo "$schedule2 $command2"; } >> "$HOME/crontab.txt"
+{ crontab -l 2>/dev/null; echo "$schedule3 $command3"; } >> "$HOME/crontab.txt"
+
+# Install the updated crontab file
+crontab "$HOME/crontab.txt"
+rm "$HOME/crontab.txt"
 
 echo "Cron jobs have been set up:"
 crontab -l
