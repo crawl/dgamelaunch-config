9adcf69b976f5befb945f1dcc29296040b2f6fbf
Rytis Petronis <petronisrytis@gmail.com>
2024-02-13 16:24:56 +0200

    update setup-cron.sh to actually auto update

--- utils/testing-container/setup-cron.sh
+++ utils/testing-container/setup-cron.sh
@@ -7,7 +7,7 @@ schedule1="*5 4 * * *"  # Runs daily at 04:05 AM
 command2="/home/crawl-dev/dgamelaunch-config/bin/dgl update-gcc stoatsoup stoatsoup/master"
 schedule2="5 3 * * *"     # Runs daily at 03:05 AM
 
-command3="/home/crawl-dev/dgamelaunch-config/bin/dgl update-gcc bcadrencrawl bcadrencrawl/bcrawl"
+command3="/home/crawl-dev/dgamelaunch-config/bin/dgl update-gcc bcadrencrawl bcadrencrawl/bCrawl"
 schedule3="35 3 * * *"      # Runs daily at 03:35 AM
 
 # Check if a crontab file exists for the user, create one if not
@@ -20,6 +20,7 @@ fi
 { crontab -l 2>/dev/null; echo "$schedule2 $command2"; } >> "$HOME/crontab.txt"
 { crontab -l 2>/dev/null; echo "$schedule3 $command3"; } >> "$HOME/crontab.txt"
 
+crontab -r
 # Install the updated crontab file
 crontab "$HOME/crontab.txt"
 rm "$HOME/crontab.txt"
