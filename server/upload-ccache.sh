0a2842911fe4a83171492f96fdec699367745293
refracta <58779799+refracta@users.noreply.github.com>
2024-05-27 16:10:41 +0900

    Add title argument

--- server/upload-ccache.sh
+++ server/upload-ccache.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+title=${1:-build-ccache}
+
 tar -czf data/ccache.tar.gz -C data ccache
 
 split -b 100M data/ccache.tar.gz data/ccache_part_
 
 tag=$(date +v%y%m%d%H%M)
 
-gh release create $tag -R refracta/dcss-webtiles-server -t build-ccache -B ccache -n ""
+gh release create $tag -R refracta/dcss-webtiles-server --title "$title" --target ccache -n ""
 
 gh release upload $tag data/ccache_part_* -R refracta/dcss-webtiles-server
 
