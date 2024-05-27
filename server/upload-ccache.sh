85528b2acba8a020f0f2fc37ff77a7a1db0abf03
refracta <refracta@abstr.net>
2024-05-27 09:40:30 +0900

    Minor fix

--- server/upload-ccache.sh
+++ server/upload-ccache.sh
@@ -6,7 +6,7 @@ split -b 100M data/ccache.tar.gz data/ccache_part_
 
 tag=$(date +v%y%m%d%H%M)
 
-gh release create $tag -R refracta/dcss-webtiles-server -t build-ccache -n ""
+gh release create $tag -R refracta/dcss-webtiles-server -t build-ccache -B ccache -n ""
 
 gh release upload $tag data/ccache_part_* -R refracta/dcss-webtiles-server
 
