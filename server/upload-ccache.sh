752849fdeffbfbb2fe60b6dca8e66d43b51bc0f0
refracta <refracta@abstr.net>
2024-05-28 08:28:59 +0900

    Add UTC time flag

--- server/upload-ccache.sh
+++ server/upload-ccache.sh
@@ -6,7 +6,7 @@ tar -czf data/ccache.tar.gz -C data ccache
 
 split -b 2GiB data/ccache.tar.gz data/ccache_part_
 
-tag=$(date +v%y%m%d%H%M)
+tag=$(date -u +v%y%m%d%H%M)
 
 gh release create $tag -R refracta/dcss-webtiles-server --title "$title" --target ccache -n ""
 
