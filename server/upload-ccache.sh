ee6dfec398f37cbf303d32c3cb0a5560362e97eb
refracta <refracta@abstr.net>
2024-05-30 18:27:10 +0900

    Pipe tar and split commands

--- server/upload-ccache.sh
+++ server/upload-ccache.sh
@@ -2,9 +2,7 @@
 
 title=${1:-build-ccache}
 
-tar -czf data/ccache.tar.gz -C data ccache
-
-split -b 1GiB data/ccache.tar.gz data/ccache_part_
+tar -czf - -C data ccache | split -b 1GiB - data/ccache_part_
 
 tag=$(date -u +v%y%m%d%H%M)
 
