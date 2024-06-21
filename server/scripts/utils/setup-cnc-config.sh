1d11a5d5c5988090d4e9b12b98aa6be3be7078a4
refracta <refracta@abstr.net>
2024-06-21 18:24:21 +0900

    Minor fix

--- server/scripts/utils/setup-cnc-config.sh
+++ server/scripts/utils/setup-cnc-config.sh
@@ -5,4 +5,9 @@ sed -i 's|CONFIG_MORGUE_URL|https://archive.nemelex.cards/morgue/%n/|g' "$DGL_CO
 sed -i 's|CONFIG_DGL_SERVER|crawl.nemelex.cards|g' "$DGL_CONF_HOME/dgl-manage.conf"
 sed -i 's|CONFIG_WEB_SAVEDUMP_URL|https://archive.nemelex.cards/saves|g' "$DGL_CONF_HOME/dgl-manage.conf"
 cp -r $DGL_CONF_HOME/server/etc/webserver/* $WEBDIR
+sed -i 's|<script type="text/javascript">|<script type="text/javascript">
+      localStorage.DWEM_MODULES = JSON.stringify(
+        ["../modules/io-hook/index.js", "../modules/module-manager/index.js", "../modules/legacy-module-support/index.js"]
+      );
+|g' "$WEBDIR/templates/client.html"
 dgl publish --confirm > /dev/null 2>&1
--- server/scripts/utils/setup-dwem.sh
+++ server/scripts/utils/setup-dwem.sh
@@ -2,8 +2,3 @@
 
 source "$DGL_CONF_HOME/dgl-manage.conf"
 sed -i 's|<script src="/static/scripts/contrib/require.js" data-main="/static/scripts/app"></script>|<script src="https://cdn.jsdelivr.net/gh/refracta/dcss-webtiles-extension-module/loader/dwem-base-loader.js"></script>|g' "$WEBDIR/templates/client.html"
-sed -i 's|<script type="text/javascript">|<script type="text/javascript">
-      localStorage.DWEM_MODULES = JSON.stringify(
-        ["../modules/io-hook/index.js", "../modules/module-manager/index.js", "../modules/legacy-module-support/index.js"]
-      );
-|g' "$WEBDIR/templates/client.html"
