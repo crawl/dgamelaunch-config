c7b098d149d1d8fb9bb6f4b84a4988fa0f3c8a30
refracta <refracta@abstr.net>
2024-06-23 16:13:59 +0900

    Add time parameter to url

--- server/scripts/utils/setup-dwem.sh
+++ server/scripts/utils/setup-dwem.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source "$DGL_CONF_HOME/dgl-manage.conf"
-sed -i 's|<script src="/static/scripts/contrib/require.js" data-main="/static/scripts/app"></script>|<script src="https://cdn.jsdelivr.net/gh/refracta/dcss-webtiles-extension-module/loader/dwem-base-loader.js"></script>|g' "$WEBDIR/templates/client.html"
+TIMESTAMP=$(date +%s%3N)
+sed -i 's|<script src="/static/scripts/contrib/require.js" data-main="/static/scripts/app"></script>|<script src="https://cdn.jsdelivr.net/gh/refracta/dcss-webtiles-extension-module/loader/dwem-base-loader.js?t='"$TIMESTAMP"'"></script>|g' "$WEBDIR/templates/client.html"
