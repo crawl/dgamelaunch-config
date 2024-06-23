ff7573a5b2454d2d65c39bbd4940172d6846b958
refracta <refracta@abstr.net>
2024-06-23 16:32:47 +0900

    Add DWEM cache parameter

--- server/scripts/utils/setup-cnc-config.sh
+++ server/scripts/utils/setup-cnc-config.sh
@@ -6,6 +6,7 @@ sed -i 's|CONFIG_DGL_SERVER|crawl.nemelex.cards|g' "$DGL_CONF_HOME/dgl-manage.co
 sed -i 's|CONFIG_WEB_SAVEDUMP_URL|https://archive.nemelex.cards/saves|g' "$DGL_CONF_HOME/dgl-manage.conf"
 cp -r $DGL_CONF_HOME/server/etc/webserver/* $WEBDIR
 sed -i 's|<script type="text/javascript">|<script  type="text/javascript">\
+      localStorage.DWEM_CACHE_PARAMETER = "?t=" + Math.floor(Date.now() / 1000 * 60 * 5);\
       localStorage.DWEM_MODULES = JSON.stringify(\
         ["io-hook", "site-information", "module-manager", "cnc-banner", "legacy-module-support"].map(m => "../modules/" + m + "/index.js")\
       );|g' "$WEBDIR/templates/client.html"
